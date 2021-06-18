ae24e2d903ac86b3185e11c46f0c39b1aad80148
ml-oss <76426363+ml-oss@users.noreply.github.com>
2021-06-18 16:33:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,17 @@ from PIL import Image
 
 st.title("Food Image Classifier")
 
-st.write(['chicken_curry', 'chicken_wings', 'fried_rice', 'grilled_salmon',
-       'hamburger', 'ice_cream', 'pizza', 'ramen', 'steak', 'sushi'] "can be predicted")
+st.write("chicken_curry, chicken_wings, fried rice, grilled salmon, hamburger, ice_cream, pizza, ramen, steak, sushi")
+
+st.write("These food classes can be recognised by the model. Have Fun")
+     
 
 
 
-uploaded_file = st.file_uploader("img", type="jpg")
+uploaded_file = st.file_uploader("Upload Your Image JPG FORMAT ONLY ", type="jpg")
 if uploaded_file is not None:
         image = Image.open(uploaded_file)
-        st.image(image, caption='img', use_column_width=True)
+        st.image(image, caption='Your image', use_column_width=True)
         st.write("")
         st.write("Classifying...")
         label = teachable_machine_classification(image, 'model.sav')
