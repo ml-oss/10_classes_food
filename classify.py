f2c8e45818318d2529a97f3c83a22d639c17b06e
ml-oss <76426363+ml-oss@users.noreply.github.com>
2021-06-22 09:56:09 +0530

    Update classify.py

--- classify.py
+++ classify.py
@@ -1,12 +1,12 @@
 import tensorflow as tf
 from PIL import Image, ImageOps
 import numpy as np
-
-
+import numpy as np
+import matplotlib.image as imgage
 def load_and_prep_image(filename,img_shape = 224):
-    img = tf.io.read_file(filename)
-    img = tf.image.decode_image(img)
-    img = tf.image.resize(img , size = [img_shape,img_shape])
+    img = imgage.imread(filename)
+    img = np.array(img)
+    img = img.resize((1,img_shape,img_shape,3))
     img = img/255.
     return img
 
@@ -16,8 +16,8 @@ def pred_plot(filename,class_names):
     model = tf.keras.models.load_model("model.sav")
     img = load_and_prep_image(filename)
 
-    pred = model.predict(tf.expand_dims(img,axis=0))
+    pred = model.predict(img)
 
-    pred_class = class_names[tf.argmax(pred[0])]
+    pred_class = class_names[pred[0].argmax()]
     
     return pred_class
