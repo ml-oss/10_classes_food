b8da027f94a3749bfbcc877f851738964af26135
ml-oss <76426363+ml-oss@users.noreply.github.com>
2021-06-22 09:46:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ st.title("Food Image Classifier")
 st.write("chicken_curry, chicken_wings, fried rice, grilled salmon, hamburger, ice_cream, pizza, ramen, steak, sushi")
 
 st.write("These food classes can be recognised by the model. Have Fun")
+
+class_names = ['chicken_curry', 'chicken_wings', 'fried_rice', 'grilled_salmon',
+       'hamburger', 'ice_cream', 'pizza', 'ramen', 'steak', 'sushi']
      
 
 
@@ -18,29 +21,6 @@ if uploaded_file is not None:
         st.image(image, caption='Your image', use_column_width=True)
         st.write("")
         st.write("Classifying...")
-        label = teachable_machine_classification(image, 'model.sav')
-        if label == 0:
-            st.write("chicken_curry")  
-        if label == 1:
-            st.write("chicken_wings")
-        if label == 2:
-            st.write("fried_rice")
-        if label == 3:
-            st.write("grilled_salmon")
-        if label == 4:
-            st.write("hamburger")
+        label = pred_plot(image,class_names)
+        st.write(label)
 
-        if label == 5:
-            st.write("ice_cream")
-
-        if label == 6:
-            st.write("pizza")
-        
-        if label == 7:
-            st.write("ramen")
-        
-        if label == 8:
-            st.write("steak")
-
-        if label == 9:
-            st.write("sushi")
